f5713074a2747606c51fd3fffa8bbff7633a54ad
Jim Zhan <jim.zhan@me.com>
2024-02-28 15:26:07 +0800

    enable test coverage

--- vitest.config.js
+++ vitest.config.js
@@ -4,7 +4,13 @@ export default defineConfig({
   globalSetup: './vitest.setup.js',
   test: {
     watch: false,
+    include: ['./src/**/*.spec.js'],
     exclude: [...configDefaults.exclude],
-    reporters: 'verbose',
-  },
+    coverage: {
+      enabled: true,
+      provider: 'istanbul',
+      include: ['src/**'],
+      reporter: ['text', 'json', 'html']
+    }
+  }
 })
