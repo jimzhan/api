c571874589e7544ae9d47b74d14af46d93d30263
Gino Zhu <gino_zhu@epam.com>
2024-03-12 14:00:18 +0800

    replace node-rdkafka Stream API with standard API

--- src/server/kafka.spec.js
+++ src/server/kafka.spec.js
@@ -1,15 +1,35 @@
-import {
-  describe,
-  expect,
-  it
-} from 'vitest'
+import { describe, expect, it } from 'vitest'
 
 import { Producer } from './kafka.js'
 
 describe('kafka', async () => {
-  it('produce message', async () => {
-    const producer = new Producer('order')
-    const success = producer.write({ test: 'test' })
-    expect(success).toBeTruthy()
+  it('produces message to topic', async () => {
+    const producer = new Producer('a_test_topic')
+    await producer.ready()
+    expect(
+      producer.write({
+        stringType: 'string-value',
+        booleanType: true,
+        intType: 10,
+        nestedMap: { intType: 1 }
+      })
+    ).toBeTruthy()
+  })
+
+  it('throws error on invalid value schema', async () => {
+    const producer = new Producer('another_test_topic')
+    await producer.ready()
+    expect(() =>
+      producer.write({
+        notSupportedType: null
+      })
+    ).toThrowError()
+  })
+
+  it('throws error on invalid connection', async () => {
+    const producer = new Producer('a_test_topic', {
+      'metadata.broker.list': 'some_where_outer_space:443'
+    })
+    expect(producer.ready()).rejects.toBeInstanceOf(Error)
   })
 })
