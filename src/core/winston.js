33262647d2da5680b910a7033d44bae0234fb96e
Jim Zhan <jim_zhan@epam.com>
2024-04-08 16:49:38 +0800

    enabled log rotation

--- src/core/winston.js
+++ src/core/winston.js
@@ -1,12 +1,11 @@
-import path from 'node:path'
 import winston from 'winston'
-
+import Rotation from 'winston-daily-rotate-file'
 import * as fsx from './fsx.js'
 
 const basedir = fsx.dirname(import.meta, '../../logs')
+const service = process.env.npm_package_name || 'app'
 
 // @TODO https://docs.aws.amazon.com/prescriptive-guidance/latest/logging-monitoring-for-application-owners/event-attributes.html
-
 export default winston.createLogger({
   level: 'info',
   // By default winston has verbose level and does not have trace.
@@ -19,15 +18,16 @@ export default winston.createLogger({
     debug: 5
   },
   format: winston.format.json(),
-  defaultMeta: { service: process.env.npm_package_name },
+  defaultMeta: { service },
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({
-      level: 'error',
-      filename: `${path.join(basedir, 'error.log')}`
-    }),
-    new winston.transports.File({
-      filename: `${path.join(basedir, 'combined.log')}`
+    new Rotation({
+      dirname: basedir,
+      filename: `${service}.%DATE%.log`,
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: true,
+      maxSize: '10m',
+      maxFiles: '30d'
     })
   ]
 })
